636a73a6c4e7a126e63f8737ed39ad62332c7e49
Alfie Main <Alfiemain96@gmail.com>
2025-09-13 21:45:11 +0100

    Added Comments

--- Practice/ex-01-functions_and_returns.py
+++ Practice/ex-01-functions_and_returns.py
@@ -1,18 +1,18 @@
 
 
-def get_input():
+def get_input(): # Getting Input for The First and Second Values to be used in math 
     first_value = float(input("Please Give first Input: "))
     second_value = float(input("Please Give Second Value: "))
     return first_value, second_value
 
-def choose_operation_type():
+def choose_operation_type(): # Selection for the 
     print("1: Addition")
     print("2: Multiplication")
     print("3: Division")
     print("4: Subtraction")
     return (int(input("Choose What Type of Operator: ")))
 
-def operation_type_select(choose_operation, first_variable, second_variable):
+def operation_type_select(choose_operation, first_variable, second_variable): # Selection for what type of math operation to do
     if choose_operation == 1:
         return lambda: first_variable + second_variable
     elif choose_operation == 2:
@@ -24,7 +24,7 @@ def operation_type_select(choose_operation, first_variable, second_variable):
     else:
         print("Error: Value isnt Accepted: At Operation_Type_Select")
 
-def print_result(first_variable, second_variable, choose_operation,operation_result):
+def print_result(first_variable, second_variable, choose_operation,operation_result): # Printing the results of the combination numbers
     if choose_operation == 1:
         print(f"{first_variable} + {second_variable} = {operation_result()}")
     elif choose_operation == 2:
@@ -37,7 +37,7 @@ def print_result(first_variable, second_variable, choose_operation,operation_res
         pass
 
 
-def main():
+def main(): # Main loop for the Program
     first_variable, second_variable = get_input()
     choose_operation = choose_operation_type()
     operation_result = operation_type_select(choose_operation, first_variable, second_variable)
